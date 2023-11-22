6a10ba74ddd6744579e4f52543022136df57af06
lobo lepe <d.alconerolepe@gmail.com>
2023-11-22 17:25:03 -0600

    ejer2

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -6,15 +6,19 @@
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
     
-    /*std::unordered_map<string, vector<string>> m;
-    for (auto i : strings) {
-        string x(i);
-        std::sort(x.begin(), x.end());
-        m[x].push_back(i);
+    std::unordered_map<string, vector<string>> mp;
+
+    for (auto it : strings)
+    {
+        string s = it;
+        sort(s.begin(), s.end());
+        mp[s].push_back(it);
     }
-    vector<vector<string>>* vect;
-    for (auto i : m)
-        vect->push_back(i.second);*/
+
+    vector<vector<string>> v;
+    vector<vector<string>>* vf=&v;
+    for (auto i : mp)
+        vf->push_back(i.second);
     
     
     return nullptr;

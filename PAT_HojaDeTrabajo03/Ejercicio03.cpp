6a14e07bd796303492da6eb6fc943d86625fe131
lobo lepe <d.alconerolepe@gmail.com>
2023-11-22 17:49:51 -0600

    eje3

--- PAT_HojaDeTrabajo03/Ejercicio03.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio03.cpp
@@ -2,6 +2,43 @@
 
 Node<int>* Ejercicio03::copyList(Node<int>* head)
 {
-
-    return nullptr;
+    Node<int>* tmp = head;
+    Node<int>* next=nullptr;
+    //  make copy of each node,
+    // and link them together side-by-side in a 
+    // single list.
+    while (tmp != nullptr) {
+        next = tmp->next;
+        Node<int>* copy = new Node<int>();
+        copy->value = tmp->value;
+        tmp->next = copy;
+        copy->next = next;
+        tmp = next;
+    }
+    tmp = head;
+    while (tmp != nullptr) {
+        if (tmp->random != nullptr) {
+            tmp->next->random = tmp->random->next;
+        }
+        tmp = tmp->next->next;
+    }
+    // restore the original list, and extract the 
+    // copy list.
+    tmp = head;
+    Node<int>* tmp2 = new Node<int>();
+    tmp2->value = 0;
+    Node<int>* copy;
+    Node<int>*copyTail = tmp2;
+    while (tmp != nullptr) {
+        next = tmp->next->next;
+        // extract the copy
+        copy = tmp->next;
+        copyTail->next = copy;
+        copyTail = copy;
+        // restore the original list
+        tmp->next = next;
+        tmp = next;
+    }
+    return tmp2->next;
+    
 }

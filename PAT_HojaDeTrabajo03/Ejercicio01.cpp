dcc03f3c054b9c903d8262dc31b2e5fc974bc243
lobo lepe <d.alconerolepe@gmail.com>
2023-11-22 15:19:54 -0600

    caso 0

--- PAT_HojaDeTrabajo03/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio01.cpp
@@ -5,7 +5,7 @@ int Ejercicio01::findMin(int nums[], int n)
     int h = 0;
     int tail = n;
     int min = -1;
-    while (h < tail)
+    while (h <= tail)
     {
         int mid = h + (h - tail) / 2;
         if (mid==0||nums[mid-1]>nums[mid])

8948a7ffbec6893f365f00dea85e02b93229276f
lobo lepe <d.alconerolepe@gmail.com>
2023-11-22 18:09:03 -0600

    eje 1

--- PAT_HojaDeTrabajo03/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio01.cpp
@@ -1,34 +1,23 @@
 #include "Ejercicio01.h"
 #include<algorithm>
 
+
 int Ejercicio01::findMin(int nums[], int n)
 {
-    int l1 = -1, r1 = n - 1, l2 = -1, r2 = n;
-    while (r1 - l1 > 2 && r2 - l2 > 2)
-    {
-        if (r1 - l1 > 2) {
-            int m1 = l1 + (r1 - l1) / 3;
-            int m2 = r1 - (r1 - l1) / 3;
-            if (nums[m1] < nums[m2])
-            {
-                l1 = m1;
-            }
-            else {
-                r1 = m2;
-            }
+    int h = 0;
+    int tail = n;
+    int mid;
+    while (h < tail) {
+        mid = h + (tail - h) / 2;
+        if (nums[mid] < nums[tail]) {
+            tail = mid;
         }
-        if (r2 - l2 > 2) {
-            int m1 = l2 + (r2 - l2) / 3;
-            int m2 = r2 - (r2 - l2) / 3;
-            if (nums[m1] > nums[m2])
-            {
-                l2 = m1;
-            }
-            else {
-                r2 = m2;
-            }
+        else if (nums[mid] > nums[tail]) {
+            h = mid + 1;
         }
+        else tail--;
     }
-    return std::min(nums[r1], nums[l2 + 1]);
+    return nums[h];
+
     
 }

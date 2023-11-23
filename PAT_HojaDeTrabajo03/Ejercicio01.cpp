794f12e1846380fa02917c883dc28ac89b36b5a4
lobo lepe <d.alconerolepe@gmail.com>
2023-11-22 19:00:12 -0600

    con esto queda

--- PAT_HojaDeTrabajo03/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio01.cpp
@@ -3,14 +3,14 @@
 
 int Ejercicio01::findMin(int nums[], int n)
 {
-    int min_ele = nums[0];
+    int min = nums[0];
 
     
     for (int i = 0; i < n; i++) {
-        if (nums[i] < min_ele) {
-            min_ele = nums[i];
+        if (nums[i] < min) {
+            min = nums[i];
         }
     }
 
-    return min_ele;
+    return min;
 }

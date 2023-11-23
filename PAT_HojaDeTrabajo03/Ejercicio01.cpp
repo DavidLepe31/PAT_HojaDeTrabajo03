f62d5e1e1e5cd45503c27b63ef461f018a96a231
lobo lepe <d.alconerolepe@gmail.com>
2023-11-22 18:38:52 -0600

    ya casi

--- PAT_HojaDeTrabajo03/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio01.cpp
@@ -1,23 +1,22 @@
 #include "Ejercicio01.h"
 #include<algorithm>
 
-
 int Ejercicio01::findMin(int nums[], int n)
 {
-    int h = 0;
-    int tail = n;
-    int mid;
-    while (h < tail) {
-        mid = h + (tail - h) / 2;
-        if (nums[mid] < nums[tail]) {
-            tail = mid;
-        }
-        else if (nums[mid] > nums[tail]) {
-            h = mid + 1;
-        }
-        else tail--;
-    }
-    return nums[h];
+    int i = 0;
+    int j = n;
 
-    
+    while (i < j) {
+        int mid = (i + j) / 2;
+        if (nums[mid] < nums[j]) {
+            j = mid;
+        }
+        else {
+            i = mid + 1;
+        }
+    }
+
+    return nums[i];
 }
+    
+
